82b80c4845a19166af44b295ad4dc6c06d4d3904
Teemo Tebest <teemo.tebest@gmail.com>
2021-08-06 12:52:43 +0200

    Cleaned code

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -120,24 +120,25 @@ class App extends Component {
       .attr('x1', 0).attr('y1', y(0))
       .attr('x2', 0).attr('y2', y(-70))
     .selectAll('stop')
-      .data([{offset: '0%', color: 'rgba(255, 0, 0, 0.25)'},{offset: '30%', color: 'rgba(255, 0, 0, 0.40)'},{offset: '45%', color: 'rgba(255, 0, 0, 0.55)'},{offset: '55%', color: 'rgba(255, 0, 0, 0.70)'},{offset: '60%', color: 'rgba(255, 0, 0, 0.85)'},{offset: '100%', color: 'rgba(255, 0, 0, 1.0)'}])
-    .enter().append('stop')
-      .attr('offset', d => d.offset)
-      .attr('stop-color', d => d.color);
+      .data([{offset: '0%', color: 'rgba(255, 0, 0, 0.25)'},{offset: '30%', color: 'rgba(255, 0, 0, 0.40)'},{offset: '45%', color: 'rgba(255, 0, 0, 0.55)'},{offset: '55%', color: 'rgba(255, 0, 0, 0.70)'},{offset: '60%', color: 'rgba(255, 0, 0, 0.85)'},{offset: '100%', color: 'rgba(255, 0, 0, 1.0)'}]).enter()
+      .append('stop')
+        .attr('offset', d => d.offset)
+        .attr('stop-color', d => d.color);
 
     g.selectAll('path')
       .data([data.people_fully_vaccinated_per_hundred]).enter()
       .append('path')
-      .style('fill', 'url(#area-vaccines-' + i + ')')
-      .attr('class', style.area)
-      .attr('d', area);
+        .style('fill', 'url(#area-vaccines-' + i + ')')
+        .attr('class', style.area)
+        .attr('d', area);
 
     g.append('g')
-    .attr('class', style.xaxis)
+      .attr('class', style.xaxis)
       .attr('transform', 'translate(0,' + y(0) + ')')
       .call(d3.axisBottom(x).tickFormat(i => '')
         .tickSizeInner(0)
         .tickSizeOuter(0));
+
     g.append('g')
       .attr('class', style.yaxis)
       .call(d3.axisLeft(y)
@@ -149,33 +150,33 @@ class App extends Component {
   createLegend(g, y, x) {
     g.append('defs')
       .append('marker')
-      .attr('id', 'arrow')
-      // .attr('viewBox', [0, 0, markerBoxWidth, markerBoxHeight])
-      .attr('refX', 6)
-      .attr('refY', 6)
-      .attr('markerWidth', 30)
-      .attr('markerHeight', 30)
-      .attr('markerUnits','userSpaceOnUse')
-      .attr('orient', 'auto')
+        .attr('id', 'arrow')
+        .attr('refX', 6)
+        .attr('refY', 6)
+        .attr('markerWidth', 30)
+        .attr('markerHeight', 30)
+        .attr('markerUnits','userSpaceOnUse')
+        .attr('orient', 'auto')
       .append('path')
-      .attr('d', 'M 0 0 12 6 0 12 3 6')
-      .attr('stroke', 'black');
+        .attr('d', 'M 0 0 12 6 0 12 3 6')
+        .attr('stroke', 'black');
+
     g.append('path')
       .attr('d', d3.line()([[x(50), y(400)], [x(240), y(500)]]))
       .attr('stroke', 'black')
       .attr('marker-end', 'url(#arrow)')
       .attr('fill', 'none');
+    g.append('text')
+      .attr('transform', 'translate(' + (x(50)) + ',' + (y(450)) + ')rotate(-' + rotate + ')')
+      .attr('class', style.legend_text)
+      .attr('text-anchor', 'start')
+      .text('Daily cases');
+
     g.append('path')
       .attr('d', d3.line()([[x(200), y(-400)], [x(430), y(-500)]]))
       .attr('stroke', 'black')
       .attr('marker-end', 'url(#arrow)')
       .attr('fill', 'none');
-
-     g.append('text')
-      .attr('transform', 'translate(' + (x(50)) + ',' + (y(450)) + ')rotate(-' + rotate + ')')
-      .attr('class', style.legend_text)
-      .attr('text-anchor', 'start')
-      .text('Daily cases');
     g.append('text')
       .attr('transform', 'translate(' + (x(200)) + ',' + (y(-350)) + ')rotate(' + rotate + ')')
       .attr('class', style.legend_text)
